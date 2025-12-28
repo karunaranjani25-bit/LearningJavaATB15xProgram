148ec810298339b3958d3bc0eba62ce1a612424d
karuna <karunaranjani25@gmail.com>
2025-12-28 21:28:56 +0530

    28th Dec 2025

---
+++ src/ex_06_TernaryOperator/Lab067_To_Three_Max.java
@@ -0,0 +1,14 @@
+package ex_06_TernaryOperator;
+
+public class Lab067_To_Three_Max {
+
+    public static void main(String[] args) {
+
+        int n1 = 2;
+        int n2 = 9;
+        int n3 = -11;
+
+        System.out.println("MAX OUT OF THREE: " +((n1>n2)?n1:((n2>n3)?n2:n3)));
+
+    }
+}
---
+++ src/ex_07_ConditionalStatements/Lab068_TriangleClassifier.java
@@ -0,0 +1,28 @@
+package ex_07_ConditionalStatements;
+
+import java.util.Scanner;
+
+public class Lab068_TriangleClassifier {
+    public static void main(String[] args){
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Side1 of triangle: ");
+        int s1 = s.nextInt();
+
+        System.out.println("Enter Side2 of triangle: ");
+        int s2 = s.nextInt();
+
+        System.out.println("Enter Side3 of triangle: ");
+        int s3 = s.nextInt();
+
+        if(s1==s2 && s2==s3){
+            System.out.println("Equilateral triangle");
+        }
+        else if (s1==s2 || s2==s3 || s3==s1){
+            System.out.println("Isosceles triangle");
+        }
+        else {
+            System.out.println("Scalene triangle");
+        }
+    }
+}
